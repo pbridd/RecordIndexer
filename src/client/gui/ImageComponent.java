7987ac6d3d01fdca21b6544de530eb41831a639b
eaglewyng <parker.ridd@byu.edu>
2014-04-03 23:10:00 -0600

    Progress in implementation of Image Panel

---
+++ src/client/gui/ImageComponent.java
@@ -0,0 +1,215 @@
+package client.gui;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics2D;
+import java.awt.Image;
+import java.awt.Toolkit;
+import java.awt.event.ComponentAdapter;
+import java.awt.event.ComponentEvent;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseWheelEvent;
+import java.awt.geom.AffineTransform;
+import java.awt.geom.NoninvertibleTransformException;
+import java.awt.geom.Point2D;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+
+import javax.imageio.ImageIO;
+import javax.swing.JComponent;
+
+import client.gui.synchronization.BatchState;
+import client.gui.synchronization.BatchStateListener;
+
+
+public class ImageComponent extends JComponent implements BatchStateListener{
+
+	/**
+	 * Automatically added by Eclipse
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	private static Image NULL_IMAGE = new BufferedImage(10, 10, BufferedImage.TYPE_INT_ARGB);
+	
+	private int w_originX;
+	private int w_originY;
+	private double scale;
+	
+	private boolean dragging;
+	private int w_dragStartX;
+	private int w_dragStartY;
+	private int w_dragStartOriginX;
+	private int w_dragStartOriginY;
+	
+	
+	
+	
+	private BatchState bchS;
+
+	public ImageComponent(){
+		this.setPreferredSize(new Dimension(800, 800));
+		this.setMinimumSize(new Dimension(100, 100));
+		this.setMaximumSize(new Dimension(1000, 1000));
+		
+		this.setBackground(new Color(96, 96, 96));
+		w_originX = (int) this.getWidth() / 2;
+		w_originY = (int) this.getHeight() / 2;
+		
+		initDrag();
+		
+		this.addMouseListener(mouseAdapter);
+		this.addMouseMotionListener(mouseAdapter);
+		this.addComponentListener(componentAdapter);
+		
+		
+
+	}
+	
+	/**
+	 * Loads an image from the server
+	 * @param imageURL the URL of the image on the server
+	 * @return an Image object
+	 */
+	private Image loadImage(String imageURL) {
+		try {
+			URL url = new URL(imageURL);
+			return ImageIO.read(url);
+		}
+		catch (IOException e) {
+			return NULL_IMAGE;
+		}
+	}
+	
+	/**
+	 * Initializes the dragging variables to their starting values
+	 */
+	private void initDrag() {
+		dragging = false;
+		w_dragStartX = 0;
+		w_dragStartY = 0;
+		w_dragStartOriginX = 0;
+		w_dragStartOriginY = 0;
+	}
+	
+	private MouseAdapter mouseAdapter = new MouseAdapter() {
+
+		@Override
+		public void mousePressed(MouseEvent e) {
+			int d_X = e.getX();
+			int d_Y = e.getY();
+			
+			AffineTransform transform = new AffineTransform();
+			transform.scale(scale, scale);
+			transform.translate(-w_originX, -w_originY);
+			
+			Point2D d_Pt = new Point2D.Double(d_X, d_Y);
+			Point2D w_Pt = new Point2D.Double();
+			try
+			{
+				transform.inverseTransform(d_Pt, w_Pt);
+			}
+			catch (NoninvertibleTransformException ex) {
+				return;
+			}
+			int w_X = (int)w_Pt.getX();
+			int w_Y = (int)w_Pt.getY();
+			
+			boolean hitShape = false;
+			
+			Graphics2D g2 = (Graphics2D)getGraphics();
+			/*for (DrawingShape shape : shapes) {
+				if (shape.contains(g2, w_X, w_Y)) {
+					hitShape = true;
+					break;
+				}
+			}*/
+			
+			if (hitShape) {
+				dragging = true;		
+				w_dragStartX = w_X;
+				w_dragStartY = w_Y;		
+				w_dragStartOriginX = w_originX;
+				w_dragStartOriginY = w_originY;
+			}
+		}
+
+		@Override
+		public void mouseDragged(MouseEvent e) {		
+			if (dragging) {
+				int d_X = e.getX();
+				int d_Y = e.getY();
+				
+				AffineTransform transform = new AffineTransform();
+				transform.scale(scale, scale);
+				transform.translate(-w_dragStartOriginX, -w_dragStartOriginY);
+				
+				Point2D d_Pt = new Point2D.Double(d_X, d_Y);
+				Point2D w_Pt = new Point2D.Double();
+				try
+				{
+					transform.inverseTransform(d_Pt, w_Pt);
+				}
+				catch (NoninvertibleTransformException ex) {
+					return;
+				}
+				int w_X = (int)w_Pt.getX();
+				int w_Y = (int)w_Pt.getY();
+				
+				int w_deltaX = w_X - w_dragStartX;
+				int w_deltaY = w_Y - w_dragStartY;
+				
+				w_originX = w_dragStartOriginX - w_deltaX;
+				w_originY = w_dragStartOriginY - w_deltaY;
+				
+				repaint();
+			}
+		}
+
+		@Override
+		public void mouseReleased(MouseEvent e) {
+			initDrag();
+		}
+
+		@Override
+		public void mouseWheelMoved(MouseWheelEvent e) {
+			return;
+		}	
+	};
+	
+	private ComponentAdapter componentAdapter = new ComponentAdapter() {
+
+		@Override
+		public void componentHidden(ComponentEvent e) {
+			return;
+		}
+
+		@Override
+		public void componentMoved(ComponentEvent e) {
+			return;
+		}
+
+		@Override
+		public void componentResized(ComponentEvent e) {
+			//updateTextShapes();
+		}
+
+		@Override
+		public void componentShown(ComponentEvent e) {
+			return;
+		}
+	};
+
+	@Override
+	public void batchActionPerformed(BatchActions ba) {
+		//Loads the image from the server if the image has changed
+		if(ba == BatchActions.IMAGEHASCHANGED){
+			//TODO implement the rest of this
+			Image image = loadImage(bchS.getImagePath());
+		}
+		
+	}
+	
+}
