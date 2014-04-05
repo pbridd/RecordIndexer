b8367d07dc329419e783997142f1933ca1a85ec0
eaglewyng <parker.ridd@byu.edu>
2014-04-05 14:56:02 -0600

    Basic implementation of FormEntry

--- src/client/gui/FormEntryPanel.java
+++ src/client/gui/FormEntryPanel.java
@@ -1,20 +1,28 @@
 package client.gui;
 
 import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.List;
 
+import javax.swing.BoxLayout;
 import javax.swing.DefaultListModel;
+import javax.swing.JLabel;
 import javax.swing.JList;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
+import javax.swing.JTextField;
 
 import client.gui.synchronization.BatchState;
 import client.gui.synchronization.BatchStateListener;
 
 @SuppressWarnings("serial")
-public class FormEntryPanel extends JPanel implements BatchStateListener {
+public class FormEntryPanel extends JPanel implements BatchStateListener, ActionListener {
 	
 	BatchState bchS;
-	JList recordList;
+	JList<String> recordList;
+    List<JTextField> inputFields;
 
 	
 	public FormEntryPanel(BatchState bchS){
@@ -26,21 +34,35 @@ public class FormEntryPanel extends JPanel implements BatchStateListener {
 		this.setLayout(new BorderLayout());
 		DefaultListModel<String> listModel = new DefaultListModel<String>();
 		recordList = new JList<String>(listModel);
+		recordList.setPreferredSize(new Dimension(50, this.getHeight()));
 		//add items to the list
 		
 		for(int i = 1; i <= bchS.getProject().getRecordsPerImage(); i++){
 			listModel.addElement(i+"");
 		}
 		//add this to the layout
-		this.add(recordList, BorderLayout.WEST);
+		this.add(new JScrollPane(recordList), BorderLayout.WEST);
 		
 		
 		
 		//Make forms JPanel
+		JPanel formsJPanel = new JPanel();
+		formsJPanel.setLayout(new BoxLayout(formsJPanel, BoxLayout.Y_AXIS));
 		//make the forms
 		for(int i = 0; i < bchS.getFields().size(); i++){
+			JLabel tempLabel = new JLabel(bchS.getField(i).getFieldName());
+			JTextField tempTextField = new JTextField();
+			tempTextField.addActionListener(this);
+			JPanel tempPanel = new JPanel();
+			tempPanel.setLayout(new BoxLayout(tempPanel, BoxLayout.X_AXIS));
 			
+			//layout the things we just created
+			tempPanel.add(tempLabel);
+			tempPanel.add(tempTextField);
+			formsJPanel.add(tempPanel);
 		}
+		
+		this.add(new JScrollPane(formsJPanel), BorderLayout.CENTER);
 		
 	}
 
@@ -55,6 +77,16 @@ public class FormEntryPanel extends JPanel implements BatchStateListener {
 	@Override
 	public void batchActionPerformed(BatchActions ba, int row, int col) {
 		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		for(JTextField j : inputFields){
+			if(arg0.getSource() == j){
+				//TODO implement the rest!
+			}
+		}
 		
 	}
 	
